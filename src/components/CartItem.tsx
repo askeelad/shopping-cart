69b08524ae2ad8f35f24f8cfe52987974abab940
askeelad <khairulbashar19@gmail.com>
2024-07-28 17:55:43 +0600

    add remove on cart page

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -2,11 +2,15 @@ import { formatCurrency } from "@/lib/formatters";
 import Image from "next/image";
 import { useDispatch, useSelector } from "react-redux";
 import { add, remove, deleteCartItem } from "../redux/features/cartSlice";
-import { increaseQuantity } from "../redux/features/productSlice";
+import {
+  increaseQuantity,
+  decreaseQuantity,
+} from "../redux/features/productSlice";
 import { stateProps, itemProps } from "@/types/cart";
 
 export default function CartItem({ item }: { item: itemProps }) {
   const { id, title, thumbnail, quantity, price } = item;
+  const { product } = useSelector((state: stateProps) => state.product);
   const { numberOfItem, itemArray } = useSelector(
     (state: stateProps) => state.cart
   );
@@ -20,6 +24,20 @@ export default function CartItem({ item }: { item: itemProps }) {
     dispatch(deleteCartItem(item.id));
   };
 
+  const decrease = () => {
+    dispatch(remove(item.id));
+    dispatch(increaseQuantity({ id: id, qty: 1 }));
+  };
+
+  const increase = () => {
+    product.map((p, k) => {
+      if (id == p.id && p.stock > 0) {
+        dispatch(add({ ...item, quantity: 1 }));
+        dispatch(decreaseQuantity(id));
+      }
+    });
+  };
+
   return (
     <div className="flex items-center gap-4 mb-3">
       <p className="text-4xl">
@@ -28,10 +46,25 @@ export default function CartItem({ item }: { item: itemProps }) {
       <div>
         {title} <span className="text-xs">({quantity})</span>
       </div>
+      <div className="flex justify-around items-center mt-4 mb-2 ">
+        <button
+          onClick={decrease}
+          className="hover:text-emerald-500 hover:bg-emerald-50 w-8 h-8 rounded-full transition-colors duration-500"
+        >
+          -
+        </button>
+        <button
+          onClick={increase}
+          className="hover:text-emerald-500 hover:bg-emerald-50 w-8 h-8 rounded-full transition-colors duration-500"
+        >
+          +
+        </button>
+      </div>
       <div className="ml-auto">{formatCurrency(price)}</div>
+      <div className="ml-auto">{formatCurrency(price * quantity)}</div>
       <button
         onClick={() => removeItemFromCart()}
-        className="hover:bg-emerald-50 transition-colors rounded-full duration-500 p-1"
+        className="hover:bg-red-50 transition-colors rounded-full duration-500 p-1 bg-red-400"
       >
         <Image
           alt="delete icon"
